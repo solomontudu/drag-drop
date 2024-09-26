322e8e957f85ea7b9bf6239aca4818bf6def6cf1
solomontudu <tukintudu@gmail.com>
2024-09-26 21:24:50 +0530

    did some code cleaning in the rebuildArrays function

--- script.js
+++ script.js
@@ -161,25 +161,17 @@ function hideInputBox(column) {
 
 // allow arrays to reflect drag and drop items
 function rebuildArrays() {
-  backlogListArray = [];
-  for (let i = 0; i < backlogList.children.length; i++) {
-    backlogListArray.push(backlogList.children[i].textContent);
-  }
+  backlogListArray = Array.from(backlogList.children).map((i) => i.textContent);
 
-  progressListArray = [];
-  for (let i = 0; i < progressList.children.length; i++) {
-    progressListArray.push(progressList.children[i].textContent);
-  }
+  progressListArray = Array.from(progressList.children).map(
+    (i) => i.textContent
+  );
 
-  completeListArray = [];
-  for (let i = 0; i < completeList.children.length; i++) {
-    completeListArray.push(completeList.children[i].textContent);
-  }
+  completeListArray = Array.from(completeList.children).map(
+    (i) => i.textContent
+  );
 
-  onHoldListArray = [];
-  for (let i = 0; i < onHoldList.children.length; i++) {
-    onHoldListArray.push(onHoldList.children[i].textContent);
-  }
+  onHoldListArray = Array.from(onHoldList.children).map((i) => i.textContent);
 
   updateDOM();
 }
